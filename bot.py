435fc4d9bd2466b0f3ff314d0ba4dd532d862ed8
pernandapurba <91628062+pernandapurba@users.noreply.github.com>
2021-10-01 12:18:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+API_KEY = environ.get('API_KEY', '2e182f5c6f7711522fabf0cb65f6ceccef49f9e1')
 
 bot = Client('gplink bot',
              api_id=API_ID,
@@ -19,7 +19,7 @@ bot = Client('gplink bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm GPlink bot. Just send me link and get short link")
+        "I'm SF URL Shortlink Bot. Kirim link dan dapatkan shortlink")
 
 
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
@@ -27,13 +27,13 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your [short link]({short_link})', quote=True)
+        await message.reply(f'Ini Linknya [short link]({short_link})', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
 
 async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
+    url = 'https://onsafelink.com/api'
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
